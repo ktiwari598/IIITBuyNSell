9671bf97bd54cf5eb422c8fc8cb5b9766f257de4
ktiwari598 <karantiwarionline@gmail.com>
2022-10-02 11:26:59 +0530

    Created UI for the home page

---
+++ js/script.js
@@ -0,0 +1,73 @@
+//giving underline to nav element
+const underlineNavElement = (element) => {
+    let grandParent = element.parentElement.parentElement;
+    console.log(grandParent);
+    let childrens = grandParent.children;
+    Array.from(childrens).forEach((child) => {
+        if(child.children[0].classList.contains('active')) {
+            child.children[0].classList.remove('active');
+        }
+    });
+    element.classList.add('active');
+}
+
+
+//toggling login popup
+const toggleOnLogin = () => {
+    let popup = document.getElementById('login').children[0];
+    popup.classList.add('active');
+}
+
+const toggleOffLogin = () => {
+    let popup = document.getElementById('login').children[0];
+    popup.classList.remove('active');
+}
+
+let loginBtn = document.getElementById('login-btn');
+loginBtn.addEventListener('click', toggleOnLogin);
+
+let closeBtn = document.getElementById('close-btn');
+closeBtn.addEventListener('click',toggleOffLogin);
+
+//giving background color to each feature
+const bgColors = ['#fddde4', '#cdebbc', '#d1e8f2', '#cdd4f8','#f6dbf6'];
+let feature = document.getElementById('features');
+let count = 0;
+Array.from(feature.children).forEach(child => {
+    let content = child.children[1];
+    content.style.backgroundColor = bgColors[count++];
+});
+
+//responsive navbar with hamburger icon
+// open navbar
+
+let navbar = document.getElementById('navbar');
+let bar = document.getElementById('ham-bar');
+console.log(bar);
+const toggleOnNavbar = () => {
+    console.log(navbar);
+    navbar.classList.add('active');
+}
+bar.addEventListener('click', toggleOnNavbar);
+
+//close navbar
+let close = document.getElementById('close');
+console.log(close);
+const toggleOffNavbar = () => {
+    navbar.classList.remove('active');
+}
+close.addEventListener('click', toggleOffNavbar);
+
+
+//install-app section
+
+let appleInstallBtn = document.getElementById('apple-logo');
+let googleInstallBtn = document.getElementById('google-logo');
+
+console.log(appleInstallBtn);
+
+const toggleWorkingAlert = () => {
+    alert('Sorry for the inconvenience! Working on our Mobile App!')
+}
+appleInstallBtn.addEventListener('click', toggleWorkingAlert);
+googleInstallBtn.addEventListener('click', toggleWorkingAlert);
