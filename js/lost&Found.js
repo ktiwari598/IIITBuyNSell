d6b05a4159d39cf588cdf38230eed2d437e04bad
ktiwari598 <karantiwarionline@gmail.com>
2022-10-12 10:03:17 +0530

    Added lost&Found page

---
+++ js/lost&Found.js
@@ -0,0 +1,11 @@
+const flipCard = (element) => {
+    let firstParent = element.parentElement;
+    let product = firstParent.parentElement;
+    if(firstParent.classList.contains('front-product')) {
+        product.style.transform = 'rotateY(180deg)';
+    }
+    else {
+        product.style.transform = 'rotateY(0deg)';
+    }
+    
+}
